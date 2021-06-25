f00d59b07a202f26d3681342623d5cda39496c4e
Siva Ratheesh <ratheesh93.ngl@gmail.com>
2021-06-26 04:24:15 +0530

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webrtc'));
+app.use(express.static('./dist/herokusample'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/webrtc/'}),
+    res.sendFile('index.html', {root: 'dist/herokusample/'}),
 );
 
 // Start the app by listening on the default Heroku port
